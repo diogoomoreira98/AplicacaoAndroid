f2264db33456e0149477dcdf78897c7753b5dd0b
MyWatter <diogoj98@hotmail.com>
2022-05-02 10:07:14 +0100

    pequenas musanças em layout

--- app/src/main/java/com/example/myapplication/Inicio/Fragmentos/Reservas.java
+++ app/src/main/java/com/example/myapplication/Inicio/Fragmentos/Reservas.java
@@ -1,26 +1,45 @@
 package com.example.myapplication.Inicio.Fragmentos;
 
+
 import android.os.Bundle;
-
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
-
-import android.text.SpannableString;
-import android.text.style.UnderlineSpan;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-
+import android.widget.ArrayAdapter;
 import com.example.myapplication.R;
-
+import java.util.ArrayList;
 
 public class Reservas extends Fragment {
+
+     SearchView searchView;
+     RecyclerView recyclerView;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reservas, container, false);
+        View view = inflater.inflate(R.layout.fragment_reservas, container, false);
+        searchView = view.findViewById(R.id.searchView);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        recyclerView = view.findViewById(R.id.listar_reservas);
+
+        return view;
     }
 }
