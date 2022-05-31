cd52d17b54577a415f28200b0b003c6a97c12df6
HugoNeves0808 <hugoneves000@gmail.com>
2022-05-31 21:36:29 +0100

    model salas

---
+++ app/src/main/java/com/example/myapplication/Inicio/models/SalaModel.java
@@ -0,0 +1,49 @@
+package com.example.myapplication.Inicio.models;
+
+public class SalaModel {
+
+        int idsala;
+        String nome;
+        String duracaolimpeza;
+        String nmaxutentes;
+        String alocacaodiaria;
+        String alocacaomaxima;
+
+        public SalaModel (int idsala, String nome, String duracaolimpeza, String nmaxutentes, String alocacaodiaria, String alocacaomaxima) {
+
+            this.idsala = idsala;
+            this.nome = nome;
+            this.duracaolimpeza = duracaolimpeza;
+            this.nmaxutentes = nmaxutentes;
+            this.alocacaodiaria = alocacaodiaria;
+            this.alocacaomaxima = alocacaomaxima;
+
+        }
+
+        public int idsala() { return idsala; }
+        public void setidsala(int idcentro) { this.idsala = idsala; }
+
+        public String getNome() {
+        return nome;
+    }
+        public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+        public String getDuracaolimpeza() {
+        return duracaolimpeza;
+    }
+        public void setDuracaolimpeza(String duracaolimpeza) { this.duracaolimpeza = duracaolimpeza; }
+
+        public String getNmaxutentes() {
+        return nmaxutentes;
+    }
+        public void setNmaxutentes(String nmaxutentes) { this.nmaxutentes = nmaxutentes; }
+
+        public String getAlocacaodiaria() { return alocacaodiaria; }
+        public void setAlocacaodiaria(String alocacaodiaria) { this.alocacaodiaria = alocacaodiaria; }
+
+        public String getAlocacaomaxima() { return alocacaomaxima; }
+        public void setAlocacaomaxima(String alocacaomaxima) { this.alocacaomaxima = alocacaomaxima; }
+
+}
